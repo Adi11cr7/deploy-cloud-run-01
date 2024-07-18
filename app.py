ddaed1c08f025e686581e50585120f1ab2b0509b
Adi11cr7 <71431299+Adi11cr7@users.noreply.github.com>
2024-07-18 12:26:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World, deploying service using githup action! Trigered by commit 2"
+    return "Hello, World, deploying service using githup action! Trigered by commit 3"
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
